e5a5a49c85f2a5ee236f4bfcdebe17285fd83046
Troy Gilbert Rice <troy.rice@samaritanministries.org>
2014-08-19 14:47:31 -0500

    first successful backend call

---
+++ MM.WebApi/Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MM.Core;
+
+namespace MM.WebApi.Controllers
+{
+    public class GameController : ApiController
+    {
+        // GET api/game/5
+        public string Get(int id)
+        {
+            var codeGen = new CodeGenerator(); // or whatever
+
+            var guess = Request.GetQueryNameValuePairs().First(nvp => nvp.Key == "guess");
+            return "BBBB";
+        }
+
+    }
+}
